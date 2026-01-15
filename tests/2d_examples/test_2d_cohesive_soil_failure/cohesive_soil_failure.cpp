228a7c33761608b8fff3791c3dc6246769984ccf
huangyw1219 <huangyw1219@gmail.com>
2026-01-16 03:35:53 +0800

    Register water velocity for coupling

--- tests/2d_examples/test_2d_cohesive_soil_failure/cohesive_soil_failure.cpp
+++ tests/2d_examples/test_2d_cohesive_soil_failure/cohesive_soil_failure.cpp
@@ -25,6 +25,7 @@ int main(int ac, char *av[])
     FluidBody water_block(sph_system, makeShared<WaterBlock>("WaterBody"));
     water_block.defineClosure<WeaklyCompressibleFluid, Viscosity>(ConstructArgs(rho0_f, c_f), mu_f);
     water_block.generateParticles<BaseParticles, Lattice>();
+    water_block.getBaseParticles().addEvolvingVariable<Vecd>("Velocity");
 
     SolidBody wall_boundary(sph_system, makeShared<WallBoundary>("WallBoundary"));
     wall_boundary.defineMaterial<Solid>();
