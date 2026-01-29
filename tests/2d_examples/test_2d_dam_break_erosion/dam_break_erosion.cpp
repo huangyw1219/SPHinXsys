aa7db1cf1c40ebdeae30d86aafcc25311af490ad
huangyw1219 <huangyw1219@gmail.com>
2026-01-30 02:17:41 +0800

    Offset water block to avoid initial overlap

--- tests/2d_examples/test_2d_dam_break_erosion/dam_break_erosion.cpp
+++ tests/2d_examples/test_2d_dam_break_erosion/dam_break_erosion.cpp
@@ -175,7 +175,7 @@ int main(int ac, char *av[])
     SimpleDynamics<NormalDirectionFromBodyShape> wall_boundary_normal_direction(wall_boundary);
     SimpleDynamics<NormalDirectionFromBodyShape> soil_boundary_normal_direction(soil_block);
     SimpleDynamics<SoilInitialCondition> soil_initial_condition(soil_block);
-    SimpleDynamics<WaterInitialCondition> water_initial_condition(water_block, 0.4);
+    SimpleDynamics<WaterInitialCondition> water_initial_condition(water_block, water_top);
     InteractionWithUpdate<LinearGradientCorrectionMatrixComplex> soil_correction_matrix(soil_block_inner, soil_block_contact);
     Dynamics1Level<continuum_dynamics::PlasticIntegration1stHalfWithWallRiemann> soil_stress_relaxation(soil_block_inner, soil_block_contact);
     Dynamics1Level<continuum_dynamics::PlasticIntegration2ndHalfWithWallRiemann> soil_density_relaxation(soil_block_inner, soil_block_contact);
